00c71a08271df3bea424bf3f7576be7ec0240afe
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-12-04 11:27:14 +0530

    added .prettierrc

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="bg-[#013B94]">
+      <nav className="mx-auto flex max-w-7xl items-center"></nav>
+    </header>
+  );
+};
+
+export default Header;
